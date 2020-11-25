697596f3f6caf9d809c61a810576a0d71d9a85e1
jongwon <wengers1004@naver.com>
2020-11-25 14:55:57 +0900

    Feat: add client side router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import App from './page/App'
+import { BrowserRouter, Route } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Route exact path="/" component={App} />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 )
