20a39d201fb46b452c844da73a01b981f62cb8a2
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-11-26 15:40:40 +0900

    Feat: add channel header info get api
    채널 헤더에 필요한 정보를 전달하는 api 구현

--- backend/controller/channel/index.js
+++ backend/controller/channel/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+const { WorkspaceUserInfo } = require('../../model/WorkspaceUserInfo')
 const { Channel } = require('../../model/Channel')
 const { ChannelConfig } = require('../../model/ChannelConfig')
+const { Chat } = require('../../model/Chat')
 
 /* GET /api/channle  get channel list  */
 router.get('/', async (req, res, next) => {
@@ -38,6 +40,39 @@ router.get('/', async (req, res, next) => {
     })
 
     res.status(200).json({ success: true, result: result })
+  } catch (err) {
+    next(err)
+  }
+})
+
+/* GET /api/channle/{channelId}/info  get channel header info  */
+router.get('/:channelId/info', async (req, res, next) => {
+  try {
+    const channelId = req.params.channelId
+
+    const pinned = await Chat.find({ pinned: true, channel: channelId }).exec()
+    const channelConfig = await ChannelConfig.find({
+      channelId: channelId,
+    }).exec()
+    const workspaceUserInfo = await WorkspaceUserInfo.find({
+      _id: { $in: channelConfig.map(el => el.workspaceUserInfoId) },
+    }).exec()
+
+    const channel = await Channel.find(
+      {
+        _id: channelId,
+      },
+      { title: 1, topic: 1, channelType: 1 },
+    ).exec()
+    const extraData = {
+      pinnedNum: pinned.length,
+      memberNum: workspaceUserInfo.length,
+      member: workspaceUserInfo,
+    }
+
+    let result = { ...JSON.parse(JSON.stringify(channel[0])), ...extraData }
+
+    res.status(200).json({ success: true, result })
   } catch (err) {
     next(err)
   }
