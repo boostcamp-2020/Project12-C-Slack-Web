d8d4f22fdea665d541d8f65d3d001fcbcc7971a3
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-11-29 21:50:27 +0900

    feat: add ChannelCard component
    채널 목록에 출력되는 컴포넌트 구현

---
+++ frontend/src/atom/ChannelCard/ChannelCard.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from 'styled-components'
+import lockImg from './locker.svg'
+import hashImg from './hash.svg'
+
+function ChannelCard(props) {
+  const { title, channelType } = props.channel
+  return (
+    <ChannelCardLabel>
+      <IconImgArea className="icon">
+        <img src={channelType == 0 ? lockImg : hashImg} />
+      </IconImgArea>
+      <TitleLabel className="channel-title">{title}</TitleLabel>
+    </ChannelCardLabel>
+  )
+}
+const ChannelCardLabel = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: baseline;
+  padding: 4px 0;
+
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.2);
+    cursor: pointer;
+  }
+`
+const IconImgArea = styled.div`
+  min-width: 12px;
+  min-height: 12px;
+  margin: 0 10px 0 15px;
+`
+
+const TitleLabel = styled.div`
+  padding-right: 15px;
+  color: #f0f0f0;
+  word-break: break-all;
+  display: inline-block;
+`
+
+export default ChannelCard
