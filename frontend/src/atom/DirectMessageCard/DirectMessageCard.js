93d845526be4d7fc863a2b5570af5fe5722805bd
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-11-30 03:14:10 +0900

    Feat: add DirectMessageCard component
    채널목록에 다이렉트 메시지임을 표시할 다이렉트메시지 카드 컴포넌트를 구현했습니다.

---
+++ frontend/src/atom/DirectMessageCard/DirectMessageCard.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function DirectMessageCard(props) {
+  const directMessage = props.directMessage
+
+  return (
+    <DirectMessageCardLabel>
+      <ImgArea>
+        {directMessage.member.length <= 1 ? (
+          <SoloImg src={directMessage.member[0].profileUrl} size="20" />
+        ) : (
+          <>
+            <SoloImg src={directMessage.member[0].profileUrl} size="13" />
+            <PeopleNum size="13">{directMessage.member.length}</PeopleNum>
+          </>
+        )}
+      </ImgArea>
+      <TitleLabel className="channel-title">
+        {directMessage.member.reduce(function (acc, curr, idx) {
+          if (idx === 0) return curr.displayName
+          return acc + ', ' + curr.displayName
+        }, '')}
+      </TitleLabel>
+    </DirectMessageCardLabel>
+  )
+}
+
+const DirectMessageCardLabel = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+  padding: 4px 0;
+
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.2);
+    cursor: pointer;
+  }
+`
+const ImgArea = styled.div`
+  position: relative;
+  width: 25px;
+  height: 20px;
+  margin: 0 10px 0 15px;
+  border-radius: 3px;
+`
+
+const SoloImg = styled.img`
+  position: absolute;
+  display: flex;
+  left: 0;
+  top: 0;
+  width: ${props => props.size}px;
+  height: ${props => props.size}px;
+  border-radius: 3px;
+`
+
+const PeopleNum = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: baseline;
+  justify-content: center;
+  right: 0;
+  bottom: 0;
+  width: ${props => props.size}px;
+  height: ${props => props.size}px;
+  font-size: 6px;
+  color: white;
+  background: black;
+  border-radius: 5px;
+`
+
+const TitleLabel = styled.div`
+  width: 100%;
+  padding-right: 15px;
+  color: #f0f0f0;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  display: inline-block;
+`
+
+export default DirectMessageCard
