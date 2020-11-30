b1edd659a6bb0ff007ff28e0b7536043300e288f
rockpell <aythffk@gmail.com>
2020-11-30 21:29:39 +0900

    Feat: login page title
    로그인 페이지에 제목과 설명을 추가하였습니다.

--- frontend/src/page/login/Login.js
+++ frontend/src/page/login/Login.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import LoginButton from '../../atom/LoginButton/LoginButton'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import styled from 'styled-components'
+import SlackIcon from '../../atom/SlackIcon'
 
 const baseURL =
   process.env.NODE_ENV === 'development'
@@ -9,16 +12,61 @@ const baseURL =
     : process.env.REACT_APP_API_URL
 
 const LoginPage = () => {
+  const history = useHistory()
   const githubIcon = <FontAwesomeIcon icon={faGithub} size="2x" />
 
+  const gohomeHandle = () => {
+    history.push('/')
+  }
   return (
     <>
-      <h1>Slack에 로그인</h1>
-      <form method="GET" action={baseURL + '/api/user/sign-in/github'}>
-        <LoginButton icon={githubIcon} label={'Login With github'} />
-      </form>
+      <LoginHeader>
+        <SlackIcon onClick={gohomeHandle} />
+      </LoginHeader>
+      <CenterDiv>
+        <MainDescription>Slack에 로그인</MainDescription>
+        <Description>사용하려는 github 계정으로 계속해 주세요.</Description>
+        <LoginDiv>
+          <form method="GET" action={baseURL + '/api/user/sign-in/github'}>
+            <LoginButton icon={githubIcon} label={'Login With github'} />
+          </form>
+        </LoginDiv>
+      </CenterDiv>
     </>
   )
 }
+
+const LoginHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  padding: 48px 0 40px;
+  width: 100%;
+  align-items: center;
+`
+
+const CenterDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+`
+
+const MainDescription = styled.h1`
+  color: #1d1c1d;
+  font-weight: 700;
+  font-size: 48px;
+  line-height: 46px;
+  text-align: center;
+  margin-bottom: 10px;
+`
+
+const Description = styled.p`
+  color: #454245;
+  margin-bottom: 36px;
+`
+
+const LoginDiv = styled.div`
+  margin: 70px 0px;
+`
 
 export default LoginPage
