072a5ce1c97fdf9c142a59d1e38995c67a048659
jongwon <wengers1004@naver.com>
2020-11-30 22:48:22 +0900

    Feat: add style constant variable

---
+++ frontend/src/constant/style.js
@@ -0,0 +1,13 @@
+const COLOR = {
+  DARK_GRAY: '#4d4c4d',
+  LIGHT_GRAY: '#616061',
+  HOVER_GRAY: '#f6f6f6',
+  GRAY: '#ddd',
+  TRANSPARENT_GRAY: 'rgba(29, 28, 29, 0.3)',
+  HOVER_TEXT_GRAY: 'rgba(29, 28, 29, 0.3)',
+  GREEN: '#007a5a',
+  HOVER_GREEN: '#148567',
+  WHITE: '#fff',
+}
+
+export { COLOR }
