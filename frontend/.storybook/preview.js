9ec5d3f8540f66b08fd8fa082d73007a2aa4ee02
rockpell <aythffk@gmail.com>
2020-11-30 17:58:04 +0900

    Feat: GlobalStyle Atom
    GlobalStyle Atom을 추가하였습니다.
    GlobalStyle Atom을 storybook의 addDecorator를 이용하여 모든 스토리에
    적용하였습니다.

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,4 +1,13 @@
+import { addDecorator } from '@storybook/react'
+import GlobalStyle from '../src/atom/GlobalStyle/GlobalStyle'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
 }
+
+addDecorator(s => (
+  <>
+    <GlobalStyle />
+    {s()}
+  </>
+))
