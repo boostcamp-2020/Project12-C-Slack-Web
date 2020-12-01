081cc46be5b9dad850fc4a9db85c5567be7f5abd
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-12-01 18:05:04 +0900

    Feat: add icon color props
    icon 색상을 지정할 수 있도록 props를 추가

--- frontend/src/atom/Icon/Icon.js
+++ frontend/src/atom/Icon/Icon.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-const Icon = ({ icon, size }) => {
+const Icon = ({ icon, size, color }) => {
   return (
     <StyledIcon size={size}>
-      <FontAwesomeIcon icon={icon} />
+      <FontAwesomeIcon icon={icon} color={color || 'black'} />
     </StyledIcon>
   )
 }
