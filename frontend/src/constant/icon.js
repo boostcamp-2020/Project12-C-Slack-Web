bc3d5dd5b5434b980d197184662276b77b9e7907
rockpell <aythffk@gmail.com>
2020-12-01 15:07:27 +0900

    Feat: github icon
    free-brands-svg-icons 패키지를 추가하였습니다.
    icon.js에 깃허브 아이콘을 추가하였습니다.

--- frontend/src/constant/icon.js
+++ frontend/src/constant/icon.js
@@ -1,5 +1,7 @@
 import { faHashtag, faLock, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 export const HASHTAG = faHashtag
 export const LOCK = faLock
 export const CLOSE = faTimes
+export const GITHUB = faGithub
