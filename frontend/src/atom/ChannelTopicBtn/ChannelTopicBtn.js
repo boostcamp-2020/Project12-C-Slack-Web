9a158484010cdbc7142d7c3635c023d57401175a
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-12-01 16:48:42 +0900

    Feat: add Edit Button on ChannelTopicBtn component
    ChannelTopicBtn에 토픽을 수정할 수 있는 edit 버튼을 추가했습니다.

--- frontend/src/atom/ChannelTopicBtn/ChannelTopicBtn.js
+++ frontend/src/atom/ChannelTopicBtn/ChannelTopicBtn.js
@@ -7,13 +7,50 @@ function ChannelTopicBtn(props) {
   return topic === null || topic === '' ? (
     <AddTopicBtn>Add a Topic</AddTopicBtn>
   ) : (
-    <div>{props.topic}</div>
+    <TopicArea>
+      <Topic>{props.topic}</Topic>
+      <EditBtn>Edit</EditBtn>
+    </TopicArea>
   )
 }
 
 const AddTopicBtn = styled.div`
   &:hover {
     color: #1264a3;
+  }
+`
+
+const Topic = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 0 3px;
+  border-radius: 4px;
+  border: 1px solid transparent;
+  &:hover {
+    border: 1px solid gray;
+  }
+`
+
+const EditBtn = styled.div`
+  display: none;
+  color: blue;
+  margin-left: 5px;
+  border: 1px solid transparent;
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
+const TopicArea = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 2px 1px;
+
+  &:hover {
+    ${EditBtn} {
+      display: flex;
+    }
   }
 `
 
