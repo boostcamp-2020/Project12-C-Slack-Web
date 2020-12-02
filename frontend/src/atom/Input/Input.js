6b64c18bb4b99f11c9652a8941a694f007f3c245
jongwon <wengers1004@naver.com>
2020-12-02 16:13:00 +0900

    Feat: add maximum length count and show icon

--- frontend/src/atom/Input/Input.js
+++ frontend/src/atom/Input/Input.js
@@ -1,14 +1,58 @@
 import React from 'react'
-
-function Input({ placeholder, handleChange, handleKey, value }) {
+import styled from 'styled-components'
+import { COLOR } from '../../constant/style'
+function Input({
+  children,
+  placeholder,
+  handleChange,
+  handleKey,
+  value,
+  maxLength,
+}) {
   return (
-    <input
-      placeholder={placeholder}
-      onChange={handleChange}
-      onKeyDown={handleKey}
-      value={value}
-    />
+    <StyledInputWrapper tabIndex="0">
+      {children}
+      <StyledInput
+        placeholder={placeholder}
+        onChange={handleChange}
+        onKeyDown={handleKey}
+        value={value}
+      />
+      {maxLength && (
+        <StyledMaxLength>{maxLength - value.length}</StyledMaxLength>
+      )}
+    </StyledInputWrapper>
   )
 }
+const StyledInputWrapper = styled.div`
+  &:focus-within {
+    outline: none;
+    box-shadow: 0 0 0 1px rgba(18, 100, 163, 1),
+      0 0 0 5px rgba(29, 155, 209, 0.3);
+    border-radius: 4px;
+    border-color: transparent;
+  }
+  &:focus-within > div {
+    display: block;
+  }
+  border: 1px solid ${COLOR.TRANSPARENT_GRAY};
+  border-radius: 4px;
+  justify-content: space-between;
+  display: flex;
+  padding: 5px;
+`
+const StyledMaxLength = styled.div`
+  display: none;
+  margin-left: 10px;
+  color: ${COLOR.DARK_GRAY};
+`
+const StyledInput = styled.input`
+  font-size: 18px;
+  line-height: 1.33334;
+  font-weight: 400;
+  width: 100%;
+  border: none;
+  outline: none;
+`
 
 export default Input
