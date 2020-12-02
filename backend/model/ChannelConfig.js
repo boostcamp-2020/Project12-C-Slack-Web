4b946c19c434bfe51bf0acfb469ad4f87e338a27
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-12-02 23:40:24 +0900

    Feat: add channelConfig static methods
    channel config에 static 메소드를 선언해 스키마 모델을 이용해 메소드를 사용할 수있도록 했습니다.

--- backend/model/ChannelConfig.js
+++ backend/model/ChannelConfig.js
@@ -29,6 +29,184 @@ const channelConfigSchema = mongoose.Schema(
   { timestamps: true },
 )
 
+channelConfigSchema.statics.getChannelHeaderInfo = async function (
+  channelId,
+  workspaceUserInfoId,
+  cb,
+) {
+  try {
+    const channelConfig = this
+    const result = await channelConfig.aggregate([
+      {
+        $match: {
+          channelId: ObjectId(channelId),
+          workspaceUserInfoId: ObjectId(workspaceUserInfoId),
+        },
+      },
+      {
+        $lookup: {
+          from: 'channels',
+          let: {
+            channelId: '$channelId',
+            workspaceUserInfoId: '$workspaceUserInfoId',
+          },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $and: [{ $eq: ['$_id', '$$channelId'] }],
+                },
+              },
+            },
+            { $project: { _id: 0 } },
+          ],
+          as: 'channelId',
+        },
+      },
+      { $unwind: '$channelId' },
+      {
+        $lookup: {
+          from: 'chats',
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $and: [{ $eq: ['$pinned', true] }],
+                },
+              },
+            },
+          ],
+          as: 'pinnedCount',
+        },
+      },
+      {
+        $set: {
+          pinnedCount: { $size: '$pinnedCount' },
+        },
+      },
+      {
+        $lookup: {
+          from: 'workspaceuserinfos',
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $and: [{ $eq: ['$_id', ObjectId(workspaceUserInfoId)] }],
+                },
+              },
+            },
+          ],
+          as: 'member',
+        },
+      },
+    ])
+    cb(null, result)
+  } catch (err) {
+    return cb(err)
+  }
+}
+
+channelConfigSchema.statics.getChannelList = async function (
+  workspaceUserInfoId,
+  cb,
+) {
+  try {
+    const channelConfig = this
+    const result = await channelConfig.aggregate([
+      {
+        $match: {
+          workspaceUserInfoId: ObjectId(workspaceUserInfoId),
+        },
+      },
+      { $project: { workspaceUserInfoId: 0 } },
+      {
+        $lookup: {
+          from: 'channels',
+          let: {
+            channel_id: '$channelId',
+          },
+          pipeline: [
+            {
+              $match: {
+                $expr: { $eq: ['$_id', '$$channel_id'] },
+              },
+            },
+            {
+              $lookup: {
+                from: 'channelconfigs',
+                let: {
+                  channel_id: '$_id',
+                  channel_type: '$channelType',
+                },
+                pipeline: [
+                  {
+                    $match: {
+                      $expr: {
+                        $and: [
+                          { $eq: ['$channelId', '$$channel_id'] },
+                          { $eq: ['$$channel_type', 2] },
+                        ],
+                      },
+                    },
+                  },
+                  {
+                    $project: {
+                      _id: 0,
+                      workspaceUserInfoId: 1,
+                    },
+                  },
+                  {
+                    $lookup: {
+                      from: 'workspaceuserinfos',
+                      let: {
+                        workspaceUserInfo_id: '$workspaceUserInfoId',
+                      },
+                      pipeline: [
+                        {
+                          $match: {
+                            $expr: { $eq: ['$_id', '$$workspaceUserInfo_id'] },
+                          },
+                        },
+                        {
+                          $project: {
+                            _id: 0,
+                            displayName: 1,
+                            profileUrl: 1,
+                            isActive: 1,
+                          },
+                        },
+                      ],
+                      as: 'user',
+                    },
+                  },
+                  {
+                    $project: {
+                      workspaceUserInfoId: 1,
+                      displayName: '$user.displayName',
+                      profileUrl: '$user.profileUrl',
+                      isActive: '$user.isActive',
+                    },
+                  },
+                  { $unwind: '$displayName' },
+                  { $unwind: '$profileUrl' },
+                  { $unwind: '$isActive' },
+                ],
+                as: 'member',
+              },
+            },
+          ],
+          as: 'channelId',
+        },
+      },
+      { $unwind: '$channelId' },
+    ])
+
+    cb(null, result)
+  } catch (err) {
+    return cb(err)
+  }
+}
+
 const ChannelConfig = mongoose.model('ChannelConfig', channelConfigSchema)
 
 module.exports = { ChannelConfig }
