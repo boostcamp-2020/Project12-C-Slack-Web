8bae4a8491892ac5b0f4b8dd3abaa272c26e653e
jongwon <wengers1004@naver.com>
2020-12-03 04:43:00 +0900

    Feat: add global state store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,9 @@
 import { atom } from 'recoil'
 
 export const workspace = atom({
-  key: 'workspaceId',
-  default: '',
+  key: 'workspace',
+  default: {
+    workspaceId: '5fc4fe2faa1ecd6a71dde1a8',
+    workspaceUseInfoId: '5fc4fe427b2d5f6ae44dc15d',
+  },
 })
