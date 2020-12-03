23e6f912e52e61717a56cd715eceb9fd8a9afa45
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-12-03 22:18:23 +0900

    Refactor: update useChannelList custom hook
    useChannelList 커스텀 훅에 중복되는 부분을 함수로 통합

--- frontend/src/hooks/useChannelList.js
+++ frontend/src/hooks/useChannelList.js
@@ -11,37 +11,29 @@ const useChannelList = () => {
     workspaceUserInfoAtom,
   )
 
-  useEffect(() => {
-    if (Object.keys(Channels).length === 0) {
-      ;(async () => {
-        try {
-          const data = await request.GET(
-            '/api/channel?workspaceUserInfoId=5fc4fe427b2d5f6ae44dc15e',
-          )
-          if (data.data.success) {
-            setWorkspaceUserInfo(data.data.result.userInfo[0])
-            setChannels(data.data.result.channelConfig)
-          } else throw '채널 목록 요청 오류'
-        } catch (err) {
-          console.error(err)
-        }
-      })()
-    }
-  }, [])
-
-  const updateChannelList = async () => {
+  const getList = async () => {
     try {
       const data = await request.GET(
         '/api/channel?workspaceUserInfoId=5fc4fe427b2d5f6ae44dc15e',
       )
       if (data.data.success) {
-        setChannels(data.data.result.channelConfig)
         setWorkspaceUserInfo(data.data.result.userInfo[0])
+        setChannels(data.data.result.channelConfig)
       } else throw '채널 목록 요청 오류'
     } catch (err) {
       console.error(err)
     }
   }
+
+  useEffect(() => {
+    if (Object.keys(Channels).length === 0) {
+      ;(async () => {
+        await getList()
+      })()
+    }
+  }, [])
+
+  const updateChannelList = async () => await getList()
 
   return [Channels, updateChannelList]
 }
