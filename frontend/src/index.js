ef9adf02138750f4904822ae8e157035b1f6e28e
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-12-03 18:49:01 +0900

    Feat: add RecoilRoot on index
    Recoil 사용을 위해 index에 RecoilRoot를 추가

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,26 +12,32 @@ import Auth from './hooks/Auth'
 import GithubOAuth from './hooks/GithubOAuth'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { RecoilRoot } from 'recoil'
 
 const App = () => {
   return (
     <React.StrictMode>
-      <ToastContainer />
-      <GlobalStyle />
-      <BrowserRouter>
-        <Route exact path="/" component={Channel} />
-        <Route exact path="/login" component={Auth(LoginPage, false)} />
-        <Route path="/github-oauth" component={GithubOAuth(LoginPage, false)} />
-        <Route
-          exact
-          path="/workspace-select"
-          component={Auth(WorkspaceSelectPage, true)}
-        />
-        <Route
-          path="/workspace/:channelId"
-          component={Auth(WorkspacePage, false)}
-        />
-      </BrowserRouter>
+      <RecoilRoot>
+        <ToastContainer />
+        <GlobalStyle />
+        <BrowserRouter>
+          <Route exact path="/" component={Channel} />
+          <Route exact path="/login" component={Auth(LoginPage, false)} />
+          <Route
+            path="/github-oauth"
+            component={GithubOAuth(LoginPage, false)}
+          />
+          <Route
+            exact
+            path="/workspace-select"
+            component={Auth(WorkspaceSelectPage, true)}
+          />
+          <Route
+            path="/workspace/:channelId"
+            component={Auth(WorkspacePage, false)}
+          />
+        </BrowserRouter>
+      </RecoilRoot>
     </React.StrictMode>
   )
 }
