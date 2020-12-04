179b76f554a69ba4872ed4f38524d47cb799beb3
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-12-05 01:29:23 +0900

    Refactor: add Icons
    컴포넌트에서 필요한 아이콘들 추가

--- frontend/src/constant/icon.js
+++ frontend/src/constant/icon.js
@@ -6,6 +6,8 @@ import {
   faInfoCircle,
   faStar as coloredStar,
   faAddressBook,
+  faEllipsisV,
+  faPlus,
 } from '@fortawesome/free-solid-svg-icons'
 import { faStar } from '@fortawesome/free-regular-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
@@ -19,3 +21,5 @@ export const INFOCIRCLE = faInfoCircle
 export const COLOREDSTAR = coloredStar
 export const STAR = faStar
 export const ADDRESSBOOK = faAddressBook
+export const ELLIPSISV = faEllipsisV
+export const PLUS = faPlus
