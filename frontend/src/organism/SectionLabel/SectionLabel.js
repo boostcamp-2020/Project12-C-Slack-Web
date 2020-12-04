c9a180c20c2da471b814bf3573ea903f410f0ed4
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-12-05 01:56:45 +0900

    Feat: show current channel
    현재 채널 라벨에 파란색 배경으로 보여주도록 변경

--- frontend/src/organism/SectionLabel/SectionLabel.js
+++ frontend/src/organism/SectionLabel/SectionLabel.js
@@ -1,15 +1,26 @@
 import React, { useState } from 'react'
+import { useParams } from 'react-router'
 import styled, { keyframes, css } from 'styled-components'
+import Icon from '../../atom/Icon'
+import { ELLIPSISV, PLUS } from '../../constant/icon'
 import { Link } from 'react-router-dom'
 import ChannelCard from '../../atom/ChannelCard'
 import DirectMessageCard from '../../atom/DirectMessageCard'
+import { faBorderNone } from '@fortawesome/free-solid-svg-icons'
+
+const ICON_SIZE = 13
 
 function SectionLabel(props) {
   const [isOpen, setIsOpen] = useState(true)
   const { sectionName, lists } = props
+  const { channelId } = useParams()
 
   const openSection = () => {
     setIsOpen(!isOpen)
+  }
+
+  const openChannelsMenu = e => {
+    e.stopPropagation()
   }
 
   const renderChannelCards =
@@ -17,19 +28,25 @@ function SectionLabel(props) {
       lists.map((list, idx) => {
         if (list.channelId.channelType === 2) {
           return (
-            <Link key={idx} to={'/workspace/' + list.channelId._id}>
-              <ChannelLabel>
+            <LinkStyle key={idx} to={'/workspace/' + list.channelId._id}>
+              <ChannelLabel
+                curr={list.channelId._id === channelId}
+                isOpen={isOpen}
+              >
                 <DirectMessageCard directMessage={list.channelId} />
               </ChannelLabel>
-            </Link>
+            </LinkStyle>
           )
         }
         return (
-          <Link key={idx} to={'/workspace/' + list.channelId._id}>
-            <ChannelLabel>
+          <LinkStyle key={idx} to={'/workspace/' + list.channelId._id}>
+            <ChannelLabel
+              curr={list.channelId._id === channelId}
+              isOpen={isOpen}
+            >
               <ChannelCard channel={list.channelId} color="white"></ChannelCard>
             </ChannelLabel>
-          </Link>
+          </LinkStyle>
         )
       })
     ) : (
@@ -42,7 +59,26 @@ function SectionLabel(props) {
         <IconArea>
           <TriangleIcon isOpen={isOpen}>▶</TriangleIcon>
         </IconArea>
-        <SectionTitle>{sectionName}</SectionTitle>
+        <SectionTitle>
+          <SectionName>{sectionName}</SectionName>
+          <ButtonRelativeArea>
+            <ButtonAbsoluteArea>
+              <ChannelSectionBtn onClick={openChannelsMenu}>
+                <Icon icon={ELLIPSISV} color="white" size={ICON_SIZE + 'px'} />
+              </ChannelSectionBtn>
+              {sectionName === 'Channels' && (
+                <ChannelSectionBtn onClick={openChannelsMenu}>
+                  <Icon icon={PLUS} color="white" size={ICON_SIZE + 'px'} />
+                </ChannelSectionBtn>
+              )}
+              {sectionName === 'Direct messages' && (
+                <ChannelSectionBtn onClick={openChannelsMenu}>
+                  <Icon icon={PLUS} color="white" size={ICON_SIZE + 'px'} />
+                </ChannelSectionBtn>
+              )}
+            </ButtonAbsoluteArea>
+          </ButtonRelativeArea>
+        </SectionTitle>
       </TitleArea>
       <ListArea isOpen={isOpen}>{renderChannelCards}</ListArea>
     </SectionLabelStyle>
@@ -52,11 +88,23 @@ function SectionLabel(props) {
 const ChannelLabel = styled.div`
   width: auto;
   padding-left: 20px;
-  margin-right: 5px;
+  cursor: pointer;
   &:hover {
-    background-color: rgba(255, 255, 255, 0.2);
-    cursor: pointer;
+    background-color: ${props => {
+      if (!props.curr) return 'rgba(255, 255, 0, 0.2)'
+      return null
+    }};
   }
+  display: ${props => {
+    if (!props.isOpen && !props.curr) {
+      return 'none'
+    }
+  }};
+  background: ${props => {
+    if (props.curr) {
+      return '#1363A2'
+    }
+  }};
 `
 
 const SectionLabelStyle = styled.div`
@@ -68,30 +116,9 @@ const SectionLabelStyle = styled.div`
   padding: 4px 0;
 `
 
-const TitleArea = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  align-items: baseline;
-  margin-left: 5px;
-  user-select: none;
-  cursor: pointer;
+const LinkStyle = styled(Link)`
+  text-decoration: none;
 `
-const openSection = keyframes`
-  0% {
-    transform: rotate( 0deg);
-  }
-  100% {
-    transform: rotate( 90deg);
-  }`
-
-const closeSection = keyframes`
-0% {
-  transform: rotate( 90deg);
-}
-100% {
-  transform: rotate( 0deg);
-}`
 
 const IconArea = styled.div`
   width: 18px;
@@ -102,8 +129,6 @@ const IconArea = styled.div`
   color: #f0f0f0;
 `
 const TriangleIcon = styled.div`
-  width: 18px;
-  height: 18px;
   width: 18px;
   height: 18px;
   text-align: center;
@@ -122,14 +147,98 @@ const TriangleIcon = styled.div`
         `}
 `
 const SectionTitle = styled.div`
+  width: calc(100% - 40px);
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+`
+
+const SectionName = styled.div`
   padding-left: 10px;
   color: #f0f0f0;
   word-break: break-all;
   display: inline-block;
 `
+
+const ChannelSectionBtn = styled.div`
+  opacity: 0;
+  width: auto;
+  min-width: 18px;
+  height: 18px;
+  display: block;
+  text-align: center;
+  color: #f0f0f0;
+  padding: 4px;
+  border-radius: 3px;
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.2);
+  }
+`
+
+const showButton = keyframes`
+  0% {
+    opacity:0;
+  }
+  50% {
+    opacity:0;
+  }
+  100% {
+    opacity:100;
+  }`
+
+const TitleArea = styled.div`
+  width: 100%;
+  padding: 2px;
+  display: flex;
+  flex-direction: row;
+  align-items: baseline;
+  user-select: none;
+
+  cursor: pointer;
+  &:hover {
+    ${ChannelSectionBtn} {
+      animation: 0.2s linear ${showButton};
+      animation-direction: alternate;
+      animation-fill-mode: forwards;
+    }
+  }
+`
+
+const ButtonRelativeArea = styled.div`
+  position: relative;
+  height: 26px;
+`
+
+const ButtonAbsoluteArea = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  display: flex;
+  flex-direction: row;
+`
+
 const ListArea = styled.div`
-  display: ${props => (props.isOpen ? 'flex' : 'none')};
+  display: flex;
   width: 100%;
   flex-direction: column;
 `
+
+const openSection = keyframes`
+  0% {
+    transform: rotate( 0deg);
+  }
+  100% {
+    transform: rotate( 90deg);
+  }`
+
+const closeSection = keyframes`
+0% {
+  transform: rotate( 90deg);
+}
+100% {
+  transform: rotate( 0deg);
+}`
+
 export default SectionLabel
