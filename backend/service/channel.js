813c99ba8b3c3658d05980ed8a82c2e5b3608c13
jongwon <wengers1004@naver.com>
2020-12-07 10:02:44 +0900

    Feat: add create channel config

--- backend/service/channel.js
+++ backend/service/channel.js
@@ -14,6 +14,12 @@ const createChannel = async params => {
       message: resMessage.DUPLICATE_VALUE_ERROR,
     }
   const result = await dbErrorHandler(() => Channel.create(params))
+  await dbErrorHandler(() =>
+    ChannelConfig.create({
+      channelId: result._id,
+      workspaceUserInfoId: result.creator,
+    }),
+  )
   return {
     code: statusCode.CREATED,
     data: result,
@@ -66,11 +72,9 @@ const getChannelListDB = async ({ workspaceUserInfoId }) => {
 
 const getChannelHeaderInfoDB = async ({ channelId, workspaceUserInfoId }) => {
   verifyRequiredParams(channelId, workspaceUserInfoId)
-
   const [result] = await dbErrorHandler(() =>
     ChannelConfig.getChannelHeaderInfo(channelId, workspaceUserInfoId),
   )
-
   return {
     code: statusCode.OK,
     result,
