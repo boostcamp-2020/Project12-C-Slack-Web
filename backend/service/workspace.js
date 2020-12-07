0fc5dd103972ffc3b8ad6f7840f9d5f268143005
rockpell <aythffk@gmail.com>
2020-12-07 13:28:15 +0900

    Feat: create channel config at craete workspace
    워크스페이스 생성시에 channel config 생성

--- backend/service/workspace.js
+++ backend/service/workspace.js
@@ -5,6 +5,7 @@ import resMessage from '../util/resMessage'
 import { verifyRequiredParams, dbErrorHandler } from '../util/'
 import { encrypt, decrypt } from '../util/encryption'
 import { Channel } from '../model/Channel'
+import { ChannelConfig } from '../model/ChannelConfig'
 const mongoose = require('mongoose')
 const ObjectId = mongoose.Types.ObjectId
 
@@ -31,6 +32,12 @@ const createWorkspace = async params => {
       creator: workspaceUserInfoData._id,
       title: params.channelName,
       channelType: 1,
+    }),
+  )
+  await dbErrorHandler(() =>
+    ChannelConfig.create({
+      channelId: ObjectId(channelData._id),
+      workspaceUserInfoId: ObjectId(channelData.creator),
     }),
   )
   await dbErrorHandler(() =>
