0bd43556275d2f9ad0ec71badcab6365fcfa99a2
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-12-09 02:18:41 +0900

    Feat: add calulateTime util
    입력된 시간으로 부터 얼마나 지났는지 계산하는 유틸 구현

---
+++ frontend/src/util/calculateTime.js
@@ -0,0 +1,21 @@
+export default function calculateTime(time) {
+  const timeData = new Date(time)
+  const now = new Date()
+  let diff = (now - timeData) / 1000
+
+  if (diff < 60) {
+    return Math.floor(diff) + ' < 1 minute ago'
+  } else if ((diff /= 60) < 60) {
+    return singularOrPlural(diff, 'minute')
+  } else if ((diff /= 60) < 24) {
+    return singularOrPlural(diff, 'hour')
+  } else {
+    diff /= 24
+    return singularOrPlural(diff, 'day')
+  }
+}
+
+const singularOrPlural = (time, text) => {
+  if (Math.floor(time) === 1) return Math.floor(time) + ` ${text} ago`
+  return Math.floor(time) + ` ${text}s ago`
+}
