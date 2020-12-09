cf5cd3b81f3b12679f1cbe5364e2ec1125b99b2d
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-12-09 21:57:38 +0900

    Feat: add ThreadReactionCard component
    ThreadReactionList에서 리액션 이모티콘과 인원을 출력하는 ThreadReactionCard 컴포넌트 구현

---
+++ frontend/src/atom/ThreadReactionCard/ThreadReactionCard.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import styled from 'styled-components'
+import { useRecoilState } from 'recoil'
+import { workspaceRecoil } from '../../store'
+
+function ThreadReactionCard({ emotion, users }) {
+  const [userInfo, setUserInfo] = useRecoilState(workspaceRecoil)
+
+  return (
+    <ThreadReactionCardStyle>
+      <EmotionArea>{emotion}</EmotionArea>
+      <UserNumArea>{users.length}</UserNumArea>
+    </ThreadReactionCardStyle>
+  )
+}
+
+const ThreadReactionCardStyle = styled.div`
+  min-width: 20px;
+  height: 20px;
+  padding: 0 7px;
+  margin-right: 10px;
+  border-radius: 20px;
+  border: 1px solid gray;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`
+const EmotionArea = styled.div`
+  display: flex;
+  line-height: 20px;
+  flex-direction: row;
+  align-items: center;
+`
+
+const UserNumArea = styled.div`
+  margin-left: 5px;
+  line-height: 20px;
+  font-size: 13px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+export default ThreadReactionCard
