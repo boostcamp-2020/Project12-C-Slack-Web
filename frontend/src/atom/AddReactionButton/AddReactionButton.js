9d32f9341b28998ae381059450b617c67f9776d7
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-12-09 23:05:11 +0900

    Feat: add AddReactionButton component
    ThreadReactionList컴포넌트에 출력되어 클릭하면 이모지 모달창을 띄우는 버튼 컴포넌트 구현

---
+++ frontend/src/atom/AddReactionButton/AddReactionButton.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+import { useRecoilState } from 'recoil'
+import { modalRecoil } from '../../store'
+import EmojiModal from '../EmojiModal'
+import Icon from '../Icon'
+import { PLUS, SMILE } from '../../constant/icon'
+import { COLOR } from '../../constant/style'
+import calcEmojiModalLocation from '../../util/calculateEmojiModalLocation'
+
+function AddReactionButton() {
+  const [modal, setModal] = useRecoilState(modalRecoil)
+
+  const sendHandler = emoji => {
+    console.log('TODO: send reaction', emoji.native)
+  }
+
+  const closeHandler = () => {
+    setModal(null)
+  }
+
+  const openEmojiModal = e => {
+    const [axisX, axisY] = calcEmojiModalLocation(e)
+
+    setModal(
+      <EmojiModal
+        sendHandler={sendHandler}
+        closeHandler={closeHandler}
+        axisX={axisX}
+        axisY={axisY}
+      />,
+    )
+  }
+
+  return (
+    <AddReactionButtonStyle onClick={openEmojiModal}>
+      <Icon icon={PLUS} size="8px" />
+      <Icon icon={SMILE} size="15px" />
+    </AddReactionButtonStyle>
+  )
+}
+
+const AddReactionButtonStyle = styled.div`
+  min-width: 25px;
+  height: 20px;
+  padding: 0 7px;
+  margin: 0 10px 5px 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 20px;
+  border: 1px solid gray;
+  color: black;
+  background: ${COLOR.HOVER_GRAY};
+`
+
+export default AddReactionButton
