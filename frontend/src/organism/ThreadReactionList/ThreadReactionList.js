6b3df1e351dda4343d668435999a244709da2e68
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-12-09 22:06:04 +0900

    Fix: ThreadReactionList flex-wrap bug fix
    많은 reaction이 있을때 flex-wrap 옵션 설정을 해주지 않아 줄바꿈이 일어나지 않는 문제 flex-wrap:wrap을 통해 수정

--- frontend/src/organism/ThreadReactionList/ThreadReactionList.js
+++ frontend/src/organism/ThreadReactionList/ThreadReactionList.js
@@ -14,6 +14,7 @@ const ThreadReactionListStyle = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
+  flex-wrap: wrap;
   flex-direction: row;
   justify-content: flex-start;
   align-items: center;
