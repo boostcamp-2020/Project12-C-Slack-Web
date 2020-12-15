cb7d59eaa43287daa92a6d201f415940f24807e1
rockpell <aythffk@gmail.com>
2020-12-15 22:18:28 +0900

    Feat: add fileUploader to MessageEditor

--- frontend/src/container/MessageEditor/MessageEditor.js
+++ frontend/src/container/MessageEditor/MessageEditor.js
@@ -1,18 +1,48 @@
-import React, { useState } from 'react'
-
+import React, { useState, useEffect } from 'react'
 import Input from '../../presenter/Input'
+import FileUploader from '../FileUploader'
+import styled from 'styled-components'
+import FilePreview from '../FilePreview'
+import ImgPreview from '../ImgPreview'
+import { isEmpty, isImage } from '../../util/index'
 
 function MessageEditor({ channelTitle, sendMessage }) {
   const [message, setMessage] = useState('')
+  const [fileData, setFileData] = useState(null)
+  const [isRender, setIsRender] = useState(false)
+
+  useEffect(() => {
+    if (fileData) setIsRender(true)
+  }, [fileData])
+
   const handleInput = e => {
     setMessage(e.target.value)
   }
   const handleKey = e => {
-    if (e.key === 'Enter' && e.target.value) {
-      sendMessage(message)
+    if (e.key === 'Enter' && (e.target.value || fileData)) {
+      sendMessage(message, fileData)
       setMessage('')
+      setFileData(null)
+      setIsRender(false)
     }
   }
+
+  const renderPreview = () => {
+    return isImage(fileData?.fileType) ? (
+      <ImgPreview
+        type="input"
+        fileId={fileData?.fileId}
+        setIsRender={setIsRender}
+      />
+    ) : (
+      <FilePreview
+        type="input"
+        fileId={fileData?.fileId}
+        setIsRender={setIsRender}
+      />
+    )
+  }
+
   return (
     <div>
       <Input
@@ -22,6 +52,10 @@ function MessageEditor({ channelTitle, sendMessage }) {
         value={message}
       />
       {/* TODO markdown, chat action 적용 필요 */}
+      <div>{isRender && renderPreview()}</div>
+      <div>
+        <FileUploader setFileData={setFileData} />
+      </div>
     </div>
   )
 }
