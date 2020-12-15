273ed6010ad235ab42a8da2854be89ad4e9bd699
rockpell <aythffk@gmail.com>
2020-12-15 20:31:03 +0900

    Feat: FileUploader hidden input
    FileUploader의 input을 보이지 않게하고 대신 아이콘 버튼을 이용하여 파일
    선택창 호출
    file size 제한

--- frontend/src/container/FileUploader/FileUploader.js
+++ frontend/src/container/FileUploader/FileUploader.js
@@ -1,30 +1,54 @@
-import React, { useState } from 'react'
+import React, { useRef } from 'react'
 import request from '../../util/request'
+import styled from 'styled-components'
+import Button from '../../presenter/Button'
+import Icon from '../../presenter/Icon'
+import { CLIP } from '../../constant/icon'
+import { toast } from 'react-toastify'
 
 const fileContentType = 'multipart/form-data'
 
-function FileUploader() {
-  const [selectedFile, setSelectedFile] = useState()
+function FileUploader({ setFileData }) {
+  const fileInput = useRef(null)
 
-  const handleFileInput = e => {
-    setSelectedFile(e.target.files[0])
+  const handleFileInput = async e => {
+    if (e.target.files[0].size > 8192000) {
+      toast.error('8MB 이하의 파일만 업로드 할 수 있습니다!')
+      return
+    }
+    await handlePost(e.target.files[0])
   }
-  const handlePost = async () => {
-    const formData = new FormData()
-    formData.append('file', selectedFile)
-    await request.POST('/api/file', formData, fileContentType)
+  const handlePost = async selectedFile => {
+    if (selectedFile) {
+      const formData = new FormData()
+      formData.append('file', selectedFile)
+      const { data } = await request.POST(
+        '/api/file',
+        formData,
+        fileContentType,
+      )
+      setFileData(data.data)
+    }
   }
 
   return (
     <>
-      <input
+      <Button type="icon" handleClick={() => fileInput.current.click()}>
+        <Icon icon={CLIP} />
+      </Button>
+      <StyeldInput
         type="file"
         name="fileData"
+        ref={fileInput}
         onChange={e => handleFileInput(e)}
-      ></input>
-      <button onClick={handlePost}>전송</button>
+      ></StyeldInput>
     </>
   )
 }
+
+const StyeldInput = styled.input`
+  width: 1px;
+  height: 1px;
+`
 
 export default FileUploader
--- frontend/src/container/FileUploader/FileUploader.stories.js
+++ frontend/src/container/FileUploader/FileUploader.stories.js
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { storiesOf } from '@storybook/react'
 import FileUploader from './FileUploader'
+import styled from 'styled-components'
+import FilePreview from '../FilePreview'
+import ImgPreview from '../ImgPreview'
+import { isEmpty, isImage } from '../../util/index'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const stories = storiesOf('Organism', module)
 
 const TestComponent = () => {
-  return <FileUploader />
+  const [fileData, setFileData] = useState(null)
+  const [isRender, setIsRender] = useState(false)
+
+  useEffect(() => {
+    if (!isEmpty(fileData)) setIsRender(true)
+  }, [fileData])
+
+  const renderPreview = () => {
+    return isImage(fileData?.fileType) ? (
+      <ImgPreview
+        type="input"
+        fileId={fileData?.fileId}
+        setIsRender={setIsRender}
+      />
+    ) : (
+      <FilePreview
+        type="input"
+        fileId={fileData?.fileId}
+        setIsRender={setIsRender}
+      />
+    )
+  }
+
+  return (
+    <>
+      <ToastContainer />
+      <StyledDiv>{isRender && renderPreview()}</StyledDiv>
+      <FileUploader setFileData={setFileData} />
+    </>
+  )
 }
 stories.add('FileUploader', () => <TestComponent />)
+
+const StyledDiv = styled.div`
+  min-width: 300px;
+  min-height: 200px;
+  background: gray;
+`
