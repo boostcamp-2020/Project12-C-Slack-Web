453128995965d66d42219bd5a8ee431f5ff11018
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-12-15 16:34:39 +0900

    Feat: add request notification permission
    notification 권한 요청 추가

--- frontend/src/page/WorkspacePage/WorkspacePage.js
+++ frontend/src/page/WorkspacePage/WorkspacePage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { useParams } from 'react-router-dom'
 import { useRecoilValue } from 'recoil'
@@ -32,7 +32,11 @@ function WorkspacePage() {
       setLineWidth(width)
     }
   }
-
+  useEffect(() => {
+    if (Notification.permission !== 'denied') {
+      Notification.requestPermission()
+    }
+  }, [])
   const switching = () => {
     switch (channelId) {
       case 'threads':
