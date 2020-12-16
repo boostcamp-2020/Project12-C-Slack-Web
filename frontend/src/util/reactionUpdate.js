9c76d18a7e5cf53af997594b65f4052297cb87bc
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-12-17 03:08:25 +0900

    Feat: add reaction realtime update util
    reaction 실시간 업데이트를 위한 유틸 추가

---
+++ frontend/src/util/reactionUpdate.js
@@ -0,0 +1,59 @@
+import produce from 'immer'
+
+export const chageReactionState = (messages, reaction) =>
+  produce(messages, draft => {
+    let done = false
+    if (reaction.type === false) return
+
+    draft.forEach(chat => {
+      if (chat._id === reaction.chatId) {
+        chat.reactions.forEach(element => {
+          if (element.emoji === reaction.emoji) {
+            if (reaction.type) {
+              element.users.push({
+                _id: reaction.workspaceUserInfoId,
+                displayName: reaction.displayName,
+              })
+              element.set = true
+            } else {
+              element.users.forEach((user, idx) => {
+                if (user._id === reaction.workspaceUserInfoId) {
+                  element.users.splice(idx, 1)
+                  element.set = false
+                }
+              })
+            }
+            done = true
+          }
+        })
+        if (!done && reaction.type === 1) {
+          chat.reactions.push({
+            emoji: reaction.emoji,
+            users: [
+              {
+                _id: reaction.workspaceUserInfoId,
+                displayName: reaction.displayName,
+              },
+            ],
+            set: true,
+          })
+        }
+      }
+    })
+  })
+
+export const hasMyReaction = (messages, workspaceUserInfo) =>
+  produce(messages, draft => {
+    draft?.forEach(message => {
+      if (message?.reactions?.length === 0) return
+      message.reactions.forEach(reaction => {
+        reaction.set = false
+        if (reaction.users.length === 0) return
+
+        const result = reaction.users.every(user => {
+          return user?._id !== workspaceUserInfo?._id
+        })
+        if (!result) reaction.set = true
+      })
+    })
+  })
