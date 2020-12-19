5abd3ca206f9ed77a95433ed6352785021138384
rockpell <aythffk@gmail.com>
2020-12-19 18:29:16 +0900

    Feat: copy to clipboard
    워크스페이스 초대 모달에 copy to clipboard 버튼 추가

--- frontend/src/container/Modal/InviteWorkspaceModal/InviteWorkspaceModal.js
+++ frontend/src/container/Modal/InviteWorkspaceModal/InviteWorkspaceModal.js
@@ -8,6 +8,7 @@ import { CLOSE } from '../../../constant/icon'
 import { COLOR } from '../../../constant/style'
 import { inviteWorkspace } from '../../../api/workspace'
 import { useParams } from 'react-router-dom'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 const baseURL =
   process.env.NODE_ENV === 'development'
@@ -16,6 +17,8 @@ const baseURL =
 
 const InviteWorkspaceModal = ({ handleClose }) => {
   const [inviteURL, setInviteURL] = useState('생성중...')
+  const [isHover, setIsHover] = useState(false)
+  const [isCopy, setIsCopy] = useState(false)
   const { workspaceId } = useParams()
 
   useEffect(() => {
@@ -25,6 +28,11 @@ const InviteWorkspaceModal = ({ handleClose }) => {
       else setInviteURL('초대 링크 생성 실패')
     })()
   }, [])
+
+  const hoverLeave = () => {
+    setIsHover(false)
+    setIsCopy(false)
+  }
 
   return (
     <>
@@ -37,6 +45,20 @@ const InviteWorkspaceModal = ({ handleClose }) => {
         </StyledModalHeader>
         <StyledModalContent>
           <StyeldTextarea value={inviteURL} readOnly></StyeldTextarea>
+          <CopyToClipboard
+            text={inviteURL}
+            onCopy={() => {
+              setIsCopy(true)
+            }}
+          >
+            <CopyDiv
+              onMouseEnter={() => setIsHover(true)}
+              onMouseLeave={hoverLeave}
+            >
+              {isHover && isCopy && <CopySpan>Copied!!!</CopySpan>}
+              <Button>Copy</Button>
+            </CopyDiv>
+          </CopyToClipboard>
         </StyledModalContent>
       </Modal>
     </>
@@ -52,12 +74,42 @@ const StyledModalContent = styled.div`
   padding: 15px;
   text-align: center;
 `
-
 const StyeldTextarea = styled.textarea`
   display: inline;
   width: 95%;
   resize: none;
   background: ${COLOR.BACKGROUNT_MODAL_GRAY};
+`
+
+const CopyDiv = styled.div`
+  position: relative;
+  display: inline-block;
+`
+
+const CopySpan = styled.span`
+  width: 140px;
+  background-color: #555;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px;
+  position: absolute;
+  z-index: 1;
+  bottom: 120%;
+  left: 50%;
+  margin-left: -75px;
+  opacity: 1;
+  transition: opacity 0.3s;
+  ::after {
+    content: '';
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    margin-left: -5px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: #555 transparent transparent;
+  }
 `
 
 export default InviteWorkspaceModal
